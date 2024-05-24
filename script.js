2b5ab302406c129dbe9246562061d4d9ec537436
Reshma PS <reshmaps156@gmail.com>
2024-05-24 22:36:44 +0530

    design modified

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Kerala&units=metric&app
                 let temp = weather.main['temp']
                 console.log(temp);
 
+                //humidity
+                let humid = weather.main['humidity']
+                console.log(humid);
+
+                //weather description
+                let weatherDescription = weather.weather[0].description
+                console.log(weatherDescription);
+
+                //wind speed
+                let windSpeed = weather.wind['speed']
+                console.log(windSpeed);
+
+                //icon
+                let weather_icon = weather.weather[0].icon
+                console.log(weather_icon);
+
+                // https://openweathermap.org/img/wn/10n@2x.png
+
 
             })
 
