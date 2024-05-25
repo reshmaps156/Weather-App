1307a9440500f6a44c21c59d5766ae4fc31220ad
Reshma PS <reshmaps156@gmail.com>
2024-05-25 22:08:08 +0530

    system data and days added in the app,weather fetched and added to the index file

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-let weather = {
-    apiKey : '214d7fcfd19cc427d1b52c92a128a686',
-    fetchWeather : function(){
-        
-    }
-}
+apiKey = '214d7fcfd19cc427d1b52c92a128a686'
+city = 'chavakkad'
+//      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+
 //data fetching
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Kerala&units=metric&appid=214d7fcfd19cc427d1b52c92a128a686').then((response)=>{
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`).then((response)=>{
             
             response.json().then((weather)=>{
                 console.log(weather);
 
                 //temperature
-                let temp = weather.main['temp']
+                let temp = weather.main['temp'].toFixed()
                 console.log(temp);
 
                 //humidity
@@ -30,7 +28,59 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Kerala&units=metric&app
                 let weather_icon = weather.weather[0].icon
                 console.log(weather_icon);
 
-                // https://openweathermap.org/img/wn/10n@2x.png
+                //feels like
+                let feels__like = weather.main['feels_like'].toFixed()
+
+                //minimum temperature
+                let min_temp = weather.main['temp_min'].toFixed()
+
+                //maximum temperature
+                let max_temp = weather.main['temp_max'].toFixed()
+
+                // https://openweathermap.org/img/wn/${weather_icon}@2x.png
+
+                temperature.innerHTML = `${temp}°C`
+                feelsLike.innerHTML = 
+                weather__description.innerHTML = `${weatherDescription}`
+                wind.innerHTML = `${windSpeed} m/s`
+                humidity.innerHTML = `${humid}%`
+                feelsLike.innerHTML = `${feels__like}°C`
+                minTemp.innerHTML = `${min_temp}°C`
+                maxTemp.innerHTML = `${max_temp} °C`
+                cityName.innerHTML = `${city}`
+
+
+                //clock
+                function getTime(){
+                    const time = new Date()
+                    console.log(time);
+                
+                    const hour = time.getHours()
+                    console.log(hour);
+                
+                    const min = time.getMinutes()
+                    console.log(min);
+                
+                    const second = time.getSeconds()
+                    console.log(second);
+                    
+                
+                    clock.innerHTML = `${hour} : ${min} :${second} ${hour>12?'PM':'AM'}`
+                
+                    setTimeout(()=>{
+                        getTime()
+                    },1000)
+                }
+                getTime()
+
+                //date
+                function get__day(){
+                    const days = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]
+                    const d = new Date()
+                    let day = days[d.getDay()]
+                    today.innerHTML = `${day}`
+                }
+                get__day()
 
 
             })
